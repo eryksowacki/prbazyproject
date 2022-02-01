479f94fc7285b7c16e93047c906916dd56bc1fd2
Eryk Sowacki <eryk.sowacki@op.pl>
2022-02-01 12:01:07 +0100

    Register form and mechanics

--- register.php
+++ register.php
@@ -1,3 +1,44 @@
 <?php
-    
+    if(empty($_POST['name']) && empty($_POST['surname']) && empty($_POST['age']) && empty($_POST['email']) && empty($_POST['password']) && empty($_POST['checkPassword']))
+    {
+        if($_POST['password'] === $_POST['checkPassword'])
+        {
+            $password = sha1($_POST['password']);
+
+            require_once 'connect.php';
+            
+            if(!$connect -> connect_errno)
+            {
+                $sql = "SELECT `email` FROM `customers` where `email` like '$_POST[email]'";
+                
+                $result = $connect -> query($sql);
+
+                if($result -> num_rows !== 0)
+                {
+                    echo "Użytkownik o takim e-mailu istnieje już w bazie!";
+                    header('Location:javascript://history.go(-1)');
+                }
+                else
+                {
+                    $sql = "INSERT INTO `customers` (`name`, `surname`, `age`, `email`, `password`) VALUES ('$_POST[name]', '$_POST[surname]', '$_POST[age]', '$_POST[email]', '$password')";
+
+                    $result = $connect -> query($sql);
+                    
+                    header('Location: index.php');
+                }
+            }
+            else
+            {
+                echo "Błędne połączenie z bazą danych.";
+            }
+        }
+        else
+        {
+            header('Location: signup.php?invalidPasswd=1');
+        }
+    }
+    else
+    {
+        header('Location:javascript://history.go(-1)');
+    }
 ?>
--- signup.php
+++ signup.php
@@ -10,13 +10,20 @@
 <body>
     <div id="baner">
         <form action="register.php" method="post">
-            Imię: <input type="text" name="name"> <br> <br>
-            Nazwisko: <input type="text" name="surname"> <br> <br>
-            Wiek: <input type="number" name="age"> <br> <br>
-            E-mail: <input type="email" name="email"> <br> <br>
-            Hasło: <input type="password" name="password"> <br> <br>
-            Potwierdź hasło: <input type="password" name="checkPassword"> <br> <br>
-            <input type="submit" value="Zaloguj się">
+            Imię: <input type="text" name="name" required> <br> <br>
+            Nazwisko: <input type="text" name="surname" required> <br> <br>
+            Wiek: <input type="number" name="age" required> <br> <br>
+            E-mail: <input type="email" name="email" required> <br> <br>
+            Hasło: <input type="password" name="password" required> <br> <br>
+            Potwierdź hasło: <input type="password" name="checkPassword" required>
+            <?php
+                if(isset($_GET['invalidPasswd']))
+                {
+                    echo "<span style='color: red'>Hasła nie są takie same!</span>";
+                }
+            ?>
+            <br>
+            <input type="submit" value="Zarejestruj się">
         </form>
     </div>
 </body>
