dbc027ad470d2caf1531ff227fe55116020beb2a
Eryk Sowacki <eryk.sowacki@op.pl>
2022-02-01 10:30:02 +0100

    Changes in index.php and login.php, adding register and signup.php

--- signup.php
+++ signup.php
@@ -4,14 +4,18 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Main page</title>
+    <title>Register page</title>
     <link rel="stylesheet" href="bootstrap.css">
 </head>
 <body>
     <div id="baner">
-        <form action="login.php" method="post">
-            Login: <input type="text" name="login"> <br> <br>
+        <form action="register.php" method="post">
+            Imię: <input type="text" name="name"> <br> <br>
+            Nazwisko: <input type="text" name="surname"> <br> <br>
+            Wiek: <input type="number" name="age"> <br> <br>
+            E-mail: <input type="email" name="email"> <br> <br>
             Hasło: <input type="password" name="password"> <br> <br>
+            Potwierdź hasło: <input type="password" name="checkPassword"> <br> <br>
             <input type="submit" value="Zaloguj się">   
         </form>
         <br>
