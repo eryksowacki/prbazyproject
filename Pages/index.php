8dd36ee9124313453c471d7b3babca9b1f47656f
Eryk Sowacki <eryk.sowacki@op.pl>
2022-02-02 18:54:06 +0100

    Repairs

--- Pages/index.php
+++ Pages/index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="pl">
 <head>
-    <link rel="stylesheet" href="./Scripts/bootstrap.css">
+    <link rel="stylesheet" href="../Scripts/bootstrap.css">
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -13,26 +13,26 @@
             <img src="./Images/logo.png" alt="logo">
         </div>
         <div id="login">
-            <form action="login.php" method="post">
+            <form action="../Scripts/login.inc.php" method="post">
                 Login: <input type="text" name="login"> <br> <br>
                 Hasło: <input type="password" name="password"> <br> <br>
-                <input type="submit" name="subimtSub" value="Zaloguj się">   
+                <input type="submit" name="submitSub" value="Zaloguj się">   
             </form>
             <br>
-            <a href="../Scripts/signup.php">Nie masz konta? Zarejestruj się!</a>
+            <a href="signup.php">Nie masz konta? Zarejestruj się!</a>
         </div>
     </div>
     <div id="navi">
-        <div class="naviplate">
-            Strona Główna
+        <div class="naviTale">
+            <a href="index.php">Strona Główna</a>
         </div>
-        <div class="naviplate">
+        <div class="naviTale">
             Trenerzy
         </div>
-        <div class="naviplate">
+        <div class="naviTale">
             Siłownie
         </div>
-        <div class="naviplate">
+        <div class="naviTale">
             Profil
         </div>
     </div>
--- Pages/signup.php
+++ Pages/signup.php
@@ -2,7 +2,7 @@
 <!DOCTYPE html>
 <html lang="pl">
 <head>
-    <link rel="stylesheet" href="bootstrap.css">
+    <link rel="stylesheet" href="../Scripts/bootstrap.css">
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -17,13 +17,20 @@
         </div>
     </nav>
     <div>
-        <form action="register.inc.php" method="post">
+        <form action="../Scripts/register.inc.php" method="post">
             Imię: <input type="text" name="name" class="a"> <br> <br>
             Nazwisko: <input type="text" name="surname" class="a"> <br> <br>
             Wiek: <input type="number" name="age" class="a"> <br> <br>
-            E-mail: <input type="email" name="email" class="a"> <br> <br>
-            Hasło: <input type="password" name="password" > <br> <br>
-            Potwierdź hasło: <input type="password" name="checkPassword" >
+            E-mail: <input type="email" name="email" class="a">
+            <?php
+                if(isset($_GET['takenEmail']))
+                {
+                    echo "<span style='color: red'>Podany e-mail istnieje już w bazie!</span>";
+                }
+            ?>
+            <br> <br>
+            Hasło: <input type="password" name="password"> <br> <br>
+            Potwierdź hasło: <input type="password" name="checkPassword">
             <?php
                 if(isset($_GET['invalidPasswd']))
                 {
