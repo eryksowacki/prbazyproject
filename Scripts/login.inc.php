9353d86b338ed49247599c4cbbe06f562a3575cf
Eryk Sowacki <eryk.sowacki@op.pl>
2022-02-02 18:52:52 +0100

    Begginer log in system

--- Scripts/login.inc.php
+++ Scripts/login.inc.php
@@ -1,8 +1,47 @@
 <?php
-    if(isset($_POST['subimtSub']) || isset($_POST['login']) && isset($_POST['password']))
+    if(!isset($_POST['submitSub']) || !isset($_POST['login']) && !isset($_POST['password']))
+    {
+        header('Location: ../Pages/index.php');
+    }
+    else
     {
         $login = $_POST['login'];
         $password = sha1($_POST['password']);
-        require_once 'connect.php'; 
+        require_once 'connect_user.php';
+
+        if($connect -> connect_errno)
+        {
+            echo "Błędne połączenie z bazą danych!";
+        }
+        else
+        {
+            $sql = "SELECT `email` FROM `users` where `email` like '$login'";
+
+            $result = $connect -> query($sql);
+
+            if($result -> num_rows == 0)
+            {
+                echo "Podany login nie istnieje w bazie danych.";
+            }
+            else
+            {
+                $sql = "SELECT `password` from `users` where `email` like '$login'";
+
+                $result = $connect -> query($sql);
+
+                if($dbPassword = $result -> fetch_assoc())
+                {
+                    if($dbPassword['password']===$password)
+                    {
+                        echo "Pomyślnie zalogowano.";
+                    }
+                    else
+                    {
+                        echo "Błędne hasło.";
+                    }
+                }
+            }
+            $connect -> close();
+        }
     }
 ?>
