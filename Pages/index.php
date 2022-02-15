373addbcf46ba39a04af135985c7f602b888ea87
Eryk Sowacki <eryk.sowacki@op.pl>
2022-02-15 14:13:29 +0100

    Moving login form and adding github link on my name

--- Pages/index.php
+++ Pages/index.php
@@ -53,40 +53,10 @@
 		</button>
 	</div>
 	<div id="progress-bar"></div>
-	
-    <div id="page" class="whole-page">
-        <div id="login">
-            <form action="..\Scripts\PHP\login.inc.php" method="post">
-                Login: <input type="text" name="login">
-                Hasło: <input type="password" name="password">
-                <input type="submit" name="submitSub" value="Zaloguj się">   
-            </form>
-            <a href="signup.php">Nie masz konta? Zarejestruj się!</a>
-        </div>
-        <div id="content" class="d-flex justify-content-center">
-            <h2>Szukaj trenera personalnego</h2>
-            <form action="index.php" method="post" class="d-flex">
-                <h4>Miasto:</h4>
-                <input type="text" placeholder="Np. Poznań" name="city">
-                <h4>Nazwa siłowni:</h4>
-                <input type="text" placeholder="Np. Fabryka Formy" name="gymName">
-                <h4>Płeć trenera:</h4>
-                <select name="trainerSex">
-                    <option value="woman">Kobieta</option>
-                    <option value="man">Mężczyzna</option>
-                </select>
-                <h4>Ocena:</h4>
-                <input type="text" placeholder="od:" name="minTrainerMark">
-                <input type="text" placeholder="do:" name="maxTrainerMark">
-                <h4>Cena:</h4>
-                <input type="text" placeholder="od" name="minPrize"> 
-                <input type="text" placeholder="do:" name="maxPrize">
-                <input type="submit" value="Szukaj">
-            </form>
-        </div>
-    </div>
+    <!-- <div id="page" class="whole-page">
+    </div> -->
     <footer id="footer">
-        <span id="footerText">Projekt Aplikacje/Bazy <i>Eryk Sowacki & <a href="https://github.com/Wichtowski">Oskar Wichtowski</a></i></span>
+        <span id="footerText">Projekt Aplikacje/Bazy <i> <a href="https://github.com/eryksowacki">Eryk Sowacki</a> & <a href="https://github.com/Wichtowski">Oskar Wichtowski</a></i></span>
     </footer>
 </body>
 <script src="..\Scripts\JS\bootstrap-5.0.2-dist\js\bootstrap.bundle.min.js" crossorigin="anonymous"></script>
--- Pages/search_trainer.php
+++ Pages/search_trainer.php
@@ -69,7 +69,27 @@
                 $connect -> close();
             }
 
-            
+        //     <div id="content" class="d-flex justify-content-center">
+        //     <h2>Szukaj trenera personalnego</h2>
+        //     <form action="search_trainer.php" method="post" class="d-flex">
+        //         <h4>Miasto:</h4>
+        //         <input type="text" placeholder="Np. Poznań" name="city">
+        //         <h4>Nazwa siłowni:</h4>
+        //         <input type="text" placeholder="Np. Fabryka Formy" name="gymName">
+        //         <h4>Płeć trenera:</h4>
+        //         <select name="trainerSex">
+        //             <option value="woman">Kobieta</option>
+        //             <option value="man">Mężczyzna</option>
+        //         </select>
+        //         <h4>Ocena:</h4>
+        //         <input type="text" placeholder="od:" name="minTrainerMark">
+        //         <input type="text" placeholder="do:" name="maxTrainerMark">
+        //         <h4>Cena:</h4>
+        //         <input type="text" placeholder="od" name="minPrize"> 
+        //         <input type="text" placeholder="do:" name="maxPrize">
+        //         <input type="submit" value="Szukaj">
+        //     </form>
+        // </div>
         ?>
     </center>
     <div id="footer">
