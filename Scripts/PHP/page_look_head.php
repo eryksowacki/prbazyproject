543014bd5b556e4e5731abd3b3bee93e5decdcdd
Eryk Sowacki <eryk.sowacki@op.pl>
2022-02-15 14:43:19 +0100

    Improvements

---
+++ Scripts/PHP/page_look_head.php
@@ -0,0 +1,6 @@
+<link rel="stylesheet" href="..\Scripts\CSS\bootstrap-5.0.2-dist\css\bootstrap.css">
+<link rel="stylesheet" href="..\Scripts\CSS\style.css">
+<link rel="shortcut icon" href="..\Images\WEBSITE IMAGES\LOGO.png" type="image/x-icon">
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
---
+++ Scripts/PHP/page_look_slider.php
@@ -0,0 +1,38 @@
+<div id="carouselExampleCaptions" class="carousel slide carousel-fade" data-bs-ride="carousel">
+		<div class="carousel-indicators">
+			<button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+			<button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+			<button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+		</div>
+		<div class="carousel-inner">
+			<div class="carousel-item active">
+			<img class="d-block w-100" src="../Images/WEBSITE IMAGES/carouserl-img1.png" alt="Obraz promocyjny siłowni. Kobiety robią martwy ciąg">
+			<div class="carousel-caption d-none d-md-block">
+				<h5>Zadbaj o swoją forme</h5>
+				<p>Z naszymi trenerami możesz osiągnąć wszystko!</p>
+			</div>
+			</div>
+			<div class="carousel-item">
+				<img class="d-block w-100" src="../Images/WEBSITE IMAGES/carouserl-img2.png" alt="Obraz promocyjny siłowni. Podgląd siłowni">
+				<div class="carousel-caption d-none d-md-block">
+				<h5>Odwiedzaj nowe siłownie!</h5>
+				<p>I znajdź tą najbardziej sprzyjającą Tobie</p>
+				</div>
+			</div>
+			<div class="carousel-item">
+				<img class="d-block w-100" src="../Images/WEBSITE IMAGES/carouserl-img3.png" alt="Obraz promocyjny siłowni. Facet martwy ciąg">
+				<div class="carousel-caption d-none d-md-block">
+					<h5>Daj z siebie 101%</h5>
+					<p>Każda przygoda zaczyna się od chęci.</p>
+				</div>
+			</div>
+		</div>
+		<button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+			<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+			<span class="visually-hidden">Previous</span>
+		</button>
+		<button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+			<span class="carousel-control-next-icon" aria-hidden="true"></span>
+			<span class="visually-hidden">Next</span>
+		</button>
+	</div>
