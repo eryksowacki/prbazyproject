23ef84d8cc3aeec57ce549c85bfabd0ca76a4f94
Oskar <oskar.wichtowski3@gmail.com>
2022-02-15 15:35:29 +0100

    search bar position fix

--- Scripts/JS/gsap-search-animation.js
+++ Scripts/JS/gsap-search-animation.js
@@ -1,17 +1,21 @@
 const search = document.querySelector('.search'); 
 search.style.display = 'none';
 
-$("#nvbrdrpdwn").on('click' ,function(){
-    if(search.style.display !=  'none'){
+$("#nvbrdrpdwn").on('click' ,function()
+{
+    if(search.style.display !=  'none')
+    {
         $("#search-bar").prop('disabled', true);
         gsap.to(search, 1, {autoAlpha:0,display:"none"});
     }
     else
     {
         $("#search-bar").prop('disabled', false);
-        gsap.to(search, 1, {autoAlpha:1,display:"block"});
+        gsap.to(search, 1, {autoAlpha:1,display:"inline"});
     }
 });
+
+
 
 
 const drop = document.querySelector(".dropdown-menu");
